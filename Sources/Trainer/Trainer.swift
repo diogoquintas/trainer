05a63c5e3ec9f425ecca5cf7351ffde13d268137
Diogo Quintas <odiogoribeiroq@gmail.com>
2026-05-10 18:31:42 +0100

    Fix top bar

--- Sources/Trainer/Trainer.swift
+++ Sources/Trainer/Trainer.swift
@@ -1,9 +1,14 @@
 import SwiftUI
+import AppKit
 
 @main
 struct Trainer: App {
     @NSApplicationDelegateAdaptor(ScreenWakeController.self) private var screenWakeController
     @StateObject private var viewModel = AppViewModel()
+
+    init() {
+        NSWindow.allowsAutomaticWindowTabbing = false
+    }
 
     var body: some Scene {
         WindowGroup {
@@ -12,5 +17,8 @@ struct Trainer: App {
                 .frame(minWidth: 1180, minHeight: 780)
         }
         .windowStyle(.hiddenTitleBar)
+        .commands {
+            CommandGroup(replacing: .newItem) {}
+        }
     }
 }
--- Sources/Trainer/Views/ContentView.swift
+++ Sources/Trainer/Views/ContentView.swift
@@ -3,18 +3,21 @@ import UniformTypeIdentifiers
 
 struct ContentView: View {
     @EnvironmentObject private var viewModel: AppViewModel
+    @State private var columnVisibility: NavigationSplitViewVisibility = .all
     @State private var isImporting = false
 
     private let zwoType = UTType(filenameExtension: "zwo") ?? .xml
 
     var body: some View {
-        NavigationSplitView {
+        NavigationSplitView(columnVisibility: $columnVisibility) {
             SidebarView(isImporting: $isImporting)
                 .frame(minWidth: 270)
         } detail: {
             WorkoutScreen(engine: viewModel.engine)
                 .id(viewModel.workout.id)
         }
+        .toolbarBackground(.visible, for: .windowToolbar)
+        .toolbarBackground(Color(nsColor: .windowBackgroundColor), for: .windowToolbar)
         .task {
             await viewModel.scanAndConnectSimulationDevices()
         }
